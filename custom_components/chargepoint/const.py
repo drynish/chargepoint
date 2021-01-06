a9170cf124d93c62fd6d8e40b1d3779b44c2b5db
Ryan Snodgrass <rsnodgrass@gmail.com>
2021-01-06 08:34:49 -0800

    added conts

---
+++ custom_components/chargepoint/const.py
@@ -0,0 +1,19 @@
+from homeassistant.const import (TIME_MINUTES, ENERGY_KILO_WATT_HOUR)
+
+CHARGEPOINT_DOMAIN = 'chargepoint'
+
+CHARGEPOINT_SERVICE = 'chargepoint_svc'
+
+ATTRIBUTION = "Data provided by Chargepoint"
+ATTR_ATTRIBUTION = 'attribute'
+
+ICON_SENSOR = "mdi:ev-station"
+ICON_SWITCH = "mdi:battery-charging"
+
+# FIXME: found these in https://github.com/home-assistant/core/blob/dev/homeassistant/components/openevse/sensor.py
+SENSOR_TYPES = {
+    "status": ["Charging Status", None],
+    "charge_time": ["Charge Time Elapsed", TIME_MINUTES],
+    "usage_session": ["Usage this Session", ENERGY_KILO_WATT_HOUR],
+    "usage_total": ["Total Usage", ENERGY_KILO_WATT_HOUR],
+}
